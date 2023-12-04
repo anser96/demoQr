cb6b18326b416ba4a782df0ad1cd848a2c98a3df
anser96 <35549012+anser96@users.noreply.github.com>
2023-12-04 18:07:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
    
-    $('#sendSmsButton').on('click', function () {
-        
+ 
+    obtenerPhone()
         getLocation();
-    });
+    
 });
 
 function getLocation() {
@@ -25,19 +25,37 @@ function getLocation() {
 function sendLocationToServer(latitude, longitude) {
     // Enviar la ubicación al servidor mediante una solicitud AJAX
     var mapsLink = 'https://www.google.com/maps?q=' + latitude + ',' + longitude;
+    let phone = localStorage.getItem('phone')
+    let telefono = '+57'+phone;
     $.ajax({
         url: 'https://demoqr-c70967ad0f23.herokuapp.com/sms',
         method: 'POST',
         contentType: 'application/json',
         data: JSON.stringify({
-            to: '+573218647995', 
-            message: 'Pare que tu paciente Tuvo un accidente Haz click en el enlace para ver la ubicación: ' + mapsLink
+            to: telefono, 
+            message: 'Parece que tu paciente Tuvo un accidente Haz click en el enlace para ver su ubicación: ' + mapsLink
         }),
         success: function (response) {
             console.log('SMS enviado exitosamente:', response);
         },
         error: function (error) {
             console.error('Error al enviar el SMS:', error);
+        }
+    });
+}
+
+function obtenerPhone(){
+    let id = obtenerIDDeURL();
+    let url = 'https://demoqr-c70967ad0f23.herokuapp.com/paciente/phone/'+id
+    $.ajax({
+        url: url,
+        method: 'GET',
+        contentType: 'application/json',
+        success: function (response) {
+            localStorage.setItem('phone',response)
+        },
+        error: function (error) {
+            console.error('Error al recibir el telefono:', error);
         }
     });
 }
